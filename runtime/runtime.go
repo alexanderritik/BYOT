0e72f68a88fb321870d9aa7040615bb316d4192d
ritik srivastava <ritik201999@gmail.com>
2026-05-02 21:55:19 +0530

    PR #9: streaming logs with timestamps via stdout/stderr pipes

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -1,8 +1,11 @@
 package runtime
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"os/exec"
+	"sync"
 	"time"
 )
 
@@ -27,7 +30,39 @@ func dockerRun(image, command, filename string, timeout int) ([]byte, error) {
 	args = append(args, "/app/binary")
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
 	defer cancel()
-	return exec.CommandContext(ctx, "docker", args...).CombinedOutput()
+
+	var wg sync.WaitGroup
+	var buf bytes.Buffer
+	var mu sync.Mutex
+	cmd := exec.CommandContext(ctx, "docker", args...)
+
+	stdoutPipe, _ := cmd.StdoutPipe()
+	stderrPipe, _ := cmd.StderrPipe()
+	cmd.Start()
+	wg.Add(1)
+	go func() {
+		defer wg.Done() // "I'm done"
+		scanner := bufio.NewScanner(stderrPipe)
+		for scanner.Scan() {
+			mu.Lock()
+			buf.WriteString(time.Now().Format(time.RFC3339) + " [stderr] " + scanner.Text() + "\n")
+			mu.Unlock()
+		}
+	}()
+
+	scanner := bufio.NewScanner(stdoutPipe)
+	for scanner.Scan() {
+		line := scanner.Text()
+		mu.Lock()
+		buf.WriteString(time.Now().Format(time.RFC3339) + " [stdout] " + line + "\n")
+		mu.Unlock()
+	}
+
+	// Wait for process to finish
+	wg.Wait()
+	cmd.Wait()
+
+	return buf.Bytes(), nil
 }
 
 func (g GoRuntime) Run(filename string, timeout int) ([]byte, error) {
--- testfunc/main.go
+++ testfunc/main.go
@@ -7,4 +7,13 @@ import (
 func main() {
 
 	fmt.Println("Hello from test function")
+
+	fmt.Println("Hello from test function")
+
+	fmt.Println("Hello from test function")
+
+	for i := 0; i <= 6; i++ {
+		fmt.Println("Hello from test function", i)
+
+	}
 }
